90ed2ece865d47aeaedd585d410a07d688c6bbd8
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-03-23 17:03:08 +0100

    make submodule-2

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-function Phone(brand, price, color, resolution, battery) {
-    this.brand = brand;
-    this.price = price;
-    this.color = color;
-    this.resolution = resolution;
-    this.battery = battery;
+function Button(text) {
+    this.text = text || 'Hello';
 }
 
-Phone.prototype.printInfo = function() {
-    console.log('The phone brand is ' + this.brand + ', the color is ' + this.color + ', and the price is ' + this.price);
-    console.log('The resolution is ' + this.resolution + 'and battery is ' + this.battery);
+Button.prototype = {
+    create: function() {
+        var self = this;
+        this.element = document.createElement('button');
+        this.element.innerText = this.text;
+        this.element.addEventListener('click', function() {
+            alert(self.text);
+        });
+        document.body.appendChild(this.element);
+    }
 }
 
-var GalaxyS6 = new Phone('samsung', 200, 'black', '5,1', '2550mAh');
-var iPhone6S = new Phone('apple', 250, 'white', '4,7', '1715mAh');
-var OnePlusOne = new Phone('one', 150, 'wood', '5,5', '3100mAh');
+var btn1 = new Button('Hello bro');
 
-OnePlusOne.printInfo();
-GalaxyS6.printInfo();
-iPhone6S.printInfo();
+btn1.create();
