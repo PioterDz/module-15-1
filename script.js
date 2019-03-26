d273fa48334b39b6536b04e26df4bbf8e9490769
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-03-26 18:09:31 +0100

    try to make new method - warrantyCost

--- script.js
+++ script.js
@@ -6,14 +6,21 @@ function Phone(brand, price, color, resolution, battery) {
     this.battery = battery;
 }
 
+Phone.prototype.warrantyCost = function() {
+    this.warrantyCost = this.price * 0.1;
+}
+
 Phone.prototype.printInfo = function() {
     console.log('The phone brand is ' + this.brand + ', the color is ' + this.color + ', and the price is ' + this.price);
     console.log('The resolution is ' + this.resolution + 'and battery is ' + this.battery);
+    console.log('The warranty cost is ' + this.warrantyCost);
 }
+
 
 var GalaxyS6 = new Phone('samsung', 200, 'black', '5,1', '2550mAh');
 var iPhone6S = new Phone('apple', 250, 'white', '4,7', '1715mAh');
 var OnePlusOne = new Phone('one', 150, 'wood', '5,5', '3100mAh');
+
 
 OnePlusOne.printInfo();
 GalaxyS6.printInfo();
